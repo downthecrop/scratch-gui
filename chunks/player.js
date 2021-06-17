2e6981dec5e50ba4a73382c15164c8214537e13a
downthecrop <bryce@mathx.ca>
2021-06-17 14:25:04 -0700

    Build for $(git log --pretty=format:%H -n1) [skip ci]

--- chunks/player.js
+++ chunks/player.js
@@ -114,6 +114,8 @@ var Player = function Player(_ref) {
   }));
 };
 
+var urlParam = new URLSearchParams(undefined.props.location.search);
+var token = urlParam.get('token');
 Player.propTypes = {
   isPlayerOnly: prop_types__WEBPACK_IMPORTED_MODULE_1___default.a.bool,
   onSeeInside: prop_types__WEBPACK_IMPORTED_MODULE_1___default.a.func,
